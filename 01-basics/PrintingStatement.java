fe5f8bec53fa20a03f0725eee91536a8c635af86
Soubhik Karmakar <soubhikk04@gmail.com>
2026-03-30 16:14:12 +0000

    Printing statement practice

---
+++ 01-basics/PrintingStatement.java
@@ -0,0 +1,7 @@
+public class PrintingStatement{
+    public static void main(String[] args) {
+        System.out.print("hello world");// it is used to print the value without moving the cursor to the next line.
+        System.out.println(" my name is soubhik");// after print it moves the cursor to the next line.
+        System.out.print("I \n love \n java");//to move the cursor to the nextline while printing the value we can use \n.
+    }
+}
